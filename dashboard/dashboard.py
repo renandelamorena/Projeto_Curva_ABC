d285a8dd314726aae7d6dc356f1eba15a0d5cee9
RenanEtq <controle.estoque.grupomaxifarma@gmail.com>
2023-11-30 17:38:05 -0300

    update dash

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -42,6 +42,7 @@ def botao_donwload(tabela_excel, nome_do_botao, nome_do_arquivo):
 st.title('Dashbord - Projeto Curva ABC') #Titulo
 
 situacao_final = pd.read_excel(r'..\tratamento_curva_abc\dados_tratados\situacao_final.xlsx').set_index('Ordem')
+local_frac = pd.read_excel(r'..\analise_curva_abc\local\datasets\local_apanha_frac.xlsx')
 
 ## Barra lateral com filtros
 
@@ -80,6 +81,7 @@ with st.sidebar:
 total_curva_frac = pd.DataFrame(situacao_final['Curva Frac'].value_counts()).reset_index().rename(columns={'Curva Frac':'Curva', 'count':'Total'})
 
 #### Seleção e tabela de itens errados com o fracionado errado:
+
 #Medicamentos curva b e c no flowrack que devem ir para a prateleira
 selecao_curva_bc_frac_errado = (situacao_final['Tipo'] == 'Flowrack') & \
                                (situacao_final['Curva Frac'].isin(['B', 'C'])) & \
@@ -137,6 +139,69 @@ curva_a_normal_flowrack_para_prateleira = situacao_final[selecao_curva_a_normal_
 
 total_curva_a_normal_prateleira_para_flowrack = curva_a_normal_flowrack_para_prateleira.shape[0]
 
+#### Tabela saída por modulo (Unidade)
+
+selecao_somente_flowrack = (situacao_final['Tipo'] == 'Flowrack') & \
+                           (situacao_final['Ender.Fracionado'] != 9010.000)
+
+somente_flowrack = situacao_final[selecao_somente_flowrack]
+
+modulos = {1:[29, 28],
+           2:[27, 26],
+           3:[25, 24],
+           4:[23, 22],
+           5:[21, 20],
+           6:[19, 18],
+           }
+
+lista_modulos = [1, 2, 3, 4, 5, 6]
+
+modulos_escolhidos = lista_modulos[:numero_modulos] #Input
+
+corredores = []
+
+for modulo in modulos_escolhidos:
+
+    corredores_modulo_atual = modulos[modulo]
+
+    corredores = corredores + corredores_modulo_atual
+
+#Modulos dos corredores
+corredor_x_modulos = {  29 : 1,
+                        28 : 1,
+                        27 : 2,
+                        26 : 2,
+                        25 : 3,
+                        24 : 3,
+                        23 : 4,
+                        22 : 4,
+                        21 : 5,
+                        20 : 5,
+                        19 : 6,
+                        18 : 6,
+                        }
+
+somente_flowrack['Ender.Fracionado'] = somente_flowrack['Ender.Fracionado'].astype(str)
+
+#Df vazio
+tabela_saida_modulo = pd.DataFrame()
+
+for corredor in corredores:
+
+    corredor_atual = somente_flowrack['Ender.Fracionado'].str.startswith(str(corredor))
+
+    saida_corredor_atual = somente_flowrack[somente_flowrack['Ender.Fracionado'].str.startswith(str(corredor))]['Qtde Venda Frac'].sum()
+
+    qnt_item_corredor_atual = somente_flowrack[somente_flowrack['Ender.Fracionado'].str.startswith(str(corredor))]['Qtde Venda Frac'].shape[0]
+
+    nova_linha = pd.Series({'Modulo': corredor_x_modulos[corredor],
+                            'Corredor': corredor,
+                            'Saída': saida_corredor_atual,
+                            'Quanidade itens' : qnt_item_corredor_atual
+                            })
+
+    tabela_saida_modulo = pd.concat([tabela_saida_modulo, nova_linha.to_frame().T], ignore_index=True)
+
 ## Graficos
 
 ### Graficos - Apanha Caixa
@@ -150,29 +215,44 @@ fig_total_curva_frac = px.bar(total_curva_frac,
                               title='Total de curvas (Fraciondo)'
                               )
 
+fig_saida_por_modulo = px.bar(tabela_saida_modulo,
+                              x='Modulo',
+                              y='Saída', 
+                              text_auto=True,
+                              title='Saída X Modulo'
+                              )
+
+fig_saida_por_corredor = px.bar(tabela_saida_modulo,
+                              x='Corredor',
+                              y='Saída', 
+                              text_auto=True,
+                              title='Saída X Corredor'
+                              )
+
+
 ## Vizualização
 
-aba1, aba2, aba3 = st.tabs(['Métricas', 'Apanha Caixa', 'Apanha Fracionado'])
+aba1, aba2, aba3 = st.tabs(['Métricas', 'Apanha Fracionado', 'Apanha Caixa'])
 
 #Métricas
 with aba1:
 
-    with st.expander('Fracionado'):
-        coluna1, coluna2, coluna3, coluna4 = st.columns(4)
-        with coluna1:   
-            st.metric('Total de itens cadastrados:', formata_numero(situacao_final['Código'].shape[0]))
-        with coluna2:
-            st.metric('Total de curvas A (Medicamentos filtrados):', formata_numero(somente_med_A.shape[0]))
 
-    coluna1, coluna2, coluna3 = st.columns(3)
+
+    coluna1, coluna2 = st.columns(2)
     with coluna1:
-        st.metric('Total de produtos inificientes (Fracionado):', formata_numero(total_curva_bc_flowrack + total_curva_a_normal_prateleira_para_flowrack + total_curva_a_normal_prateleira_para_flowrack))
+        st.metric('Produtos com endereço de fracionado ineficinente:', formata_numero(total_curva_bc_flowrack + total_curva_a_normal_prateleira_para_flowrack + total_curva_a_normal_prateleira_para_flowrack))
+        with st.expander('Fracionado'):
+            coluna1, coluna2 = st.columns(2)
+            with coluna1:
+                st.metric('Curva A "medicamento"', formata_numero(somente_med_A.shape[0]))
 
-#Apanha Caixa
-# with aba2:
+    with coluna2:
+        st.metric('Produtos com endereço de caixa fechada ineficinente:', 1)
+
 
 #Apanha Fracionado
-with aba3:
+with aba2:
 
     coluna1, coluna2, coluna3 = st.columns(3)
     with coluna1:
@@ -185,15 +265,45 @@ with aba3:
         st.metric('Curvas A do Flowrack (Na prateleira)', total_curva_a_normal_prateleira_para_flowrack)
         botao_donwload(curva_a_normal_prateleira_para_flowrack,'Donwload A - Prateleira', 'curva_a_prateleira_mudar_para_flowrack.xlsx')
 
-    #Fazer - graficos mostrando a saida por moulo (em unidade)
-
-    #Ideia - Mapa de calor com a saida por endereço do flowrack
-
-    #Fazer - grafico mostrando a saída por classe (em unidade)
-
-    modulos = {1:{'29_28':[29, 28]}
-               }
-
     coluna1, coluna2 = st.columns(2)
     with coluna1:
-        st.plotly_chart(fig_total_curva_frac, use_container_width=True)
+        st.plotly_chart(fig_saida_por_modulo, use_container_width=True)
+
+        st.markdown('# Filtar Saída pela Classe')
+
+        #Filtro por modulo
+        modulos_escolhidos = st.multiselect('Selecione os modulos:', modulos_escolhidos, default = modulos_escolhidos)
+
+        #Filtro por local
+        locais = ['AA', 'AB', 'AC', 'XPE']
+        selecao_locais = st.selectbox('Selecione os locais:', locais)
+
+        #Tabela de saido por local/classe
+        produto_flowrack = somente_flowrack[['Ender.Fracionado', 'Código', 'Qtde Venda Frac']]
+        local_frac['Ender.Fracionado'] = local_frac['Ender.Fracionado'].astype(str)
+        saida_por_local_frac = pd.merge(local_frac, produto_flowrack, on='Ender.Fracionado',  how = 'left')
+
+        #Selecionar o(s) modulo(s)
+        selecao_modulo = saida_por_local_frac['modulo'].isin(modulos_escolhidos)
+        saida_por_local_frac_modulo = saida_por_local_frac[selecao_modulo]
+
+        #Selecionar o(s) local(is)
+        selecao_local = saida_por_local_frac_modulo['local'] == selecao_locais
+        tabela_saida_por_local = saida_por_local_frac_modulo[selecao_local]    
+
+        fig_saida_por_classe = px.bar(tabela_saida_por_local,
+                               x='modulo',
+                               y='Qtde Venda Frac',
+                               text_auto=True,
+                               title='Saída X Classe'
+                               )
+
+    with coluna2:
+        st.plotly_chart(fig_saida_por_corredor, use_container_width=True)
+
+        st.plotly_chart(fig_saida_por_classe, use_container_width=True)
+
+#Ideia - Mapa de calor com a saida por endereço do flowrack
+
+#Apanha Caixa
+# with aba3:
