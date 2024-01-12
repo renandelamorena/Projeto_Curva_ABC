5c68e3d5c6ef34298d669559d03a5f8cdd1cc6c4
Renan Araújo De La Morena <renanproal@gmail.com>
2024-01-12 15:07:35 -0300

    Esqueleto do repartição de ocupação

--- pages/Ocupação.py
+++ pages/Ocupação.py
@@ -13,7 +13,15 @@ def caminho_absoluto(caminho_relativo_com_barras_normais):
 
 situacao_final = pd.read_excel(caminho_absoluto('data/tratamento_curva_abc/dados_tratados/situacao_final.xlsx')).set_index('Ordem')
 
+st.title('Ocupação do estoque')
+
 aba1, aba2, aba3 = st.tabs(['Caixa Fechada', 'Fracionado', 'Prateleira'])
 
 with aba1:
+    'a'
+    
+with aba2:
+    'a'
+    
+with aba3:
     'a'
