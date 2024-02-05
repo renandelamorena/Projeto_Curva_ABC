a903cfdd678bc9817221599256ccb594b9777d43
RenanEtq <controle.estoque.grupomaxifarma@gmail.com>
2024-02-05 14:33:18 -0300

    Ocupação de caixa fechada

--- pages/Ocupação.py
+++ pages/Ocupação.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import os
 
 def caminho_absoluto(caminho_relativo_com_barras_normais):
@@ -12,13 +13,46 @@ def caminho_absoluto(caminho_relativo_com_barras_normais):
     return caminho_absoluto
 
 situacao_final = pd.read_excel(caminho_absoluto('data/tratamento_curva_abc/dados_tratados/situacao_final.xlsx')).set_index('Ordem')
+mapa = pd.read_excel(caminho_absoluto('mapa_estoque/mapa_orientacao.xlsx')).fillna('-').astype(str)
 
+def criar_mapa_de_calor_caixa_fechada(coluna, nome_do_grafico):
+
+  coluna_x_endereço = situacao_final[['Ender.Cx.Fechada', coluna]].groupby(['Ender.Cx.Fechada']).sum().reset_index()
+  coluna_x_endereço = pd.Series(coluna_x_endereço[coluna].values, index = coluna_x_endereço['Ender.Cx.Fechada']).to_dict()
+  coluna_por_enderço_cx_fechada = mapa.replace(coluna_x_endereço)
+
+  # Criar o mapa de calor
+  fig = go.Figure(data=go.Heatmap(
+                  z=coluna_por_enderço_cx_fechada.values, # Valores para a cor
+                  x=coluna_por_enderço_cx_fechada.columns, # Eixos X
+                  y=coluna_por_enderço_cx_fechada.index, # Eixo Y
+                  colorbar_title='Saída',
+                  colorscale=['LightBlue', 'DarkBlue'],
+                  text=mapa,
+                  texttemplate='%{text}',
+                  textfont={'size':10}
+                  ))
+
+  # Ajustes finais no layout
+  fig.update_layout(title_text=nome_do_grafico,
+                    yaxis=dict(autorange='reversed'),
+                    xaxis_showgrid=False, yaxis_showgrid=False,
+                    xaxis_zeroline=False, yaxis_zeroline=False,
+                    autosize=False,
+                    width=1450,
+                    height=1200,
+                    )
+
+  fig.update_xaxes(side='top')
+  fig.show()
+  
 st.title('Ocupação do estoque')
 
 aba1, aba2, aba3 = st.tabs(['Caixa Fechada', 'Fracionado', 'Prateleira'])
 
 with aba1:
-    'a'
+    chart = criar_mapa_de_calor_caixa_fechada('Ativ.Ressupr.Frac', 'Ressuprimento fracionado por endereço')
+    st.plotly_chart(chart, use_container_width=True)
     
 with aba2:
     'a'
