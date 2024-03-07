5f15480846dc585a378148fe7c29c117ae579374
RenanEtq <controle.estoque.grupomaxifarma@gmail.com>
2024-03-07 17:51:19 -0300

    teste

--- Dashboard.py
+++ Dashboard.py
@@ -654,8 +654,6 @@ with aba2:
         colocar_local_AC = produtos_para_realocar_de_classe('AC', produtos_para_classe_AC)
         total_colocar_local_AC = len(colocar_local_AC)
 
-        produtos_para_classe_AC['Ender.Fracionado'].dtype
-
         coluna1, coluna2, coluna3 = st.columns(3)
         with coluna1:
             st.metric('Realocar para Classe AA no Flowrack', total_colocar_local_AA)
